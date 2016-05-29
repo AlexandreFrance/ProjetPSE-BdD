67ed34ddbe499dcb80a4a390d925d2fc1436afa9
AlexandreFrance <moritzalex@hotmail.fr>
2016-05-29 20:08:24 +0200

    Début

---
+++ client.h
@@ -0,0 +1,12 @@
+//Prototypes Client
+
+//connection au serveur avec son IP
+int connection(IP);
+
+
+//s'indentifier au serveur 
+int identifier(char username, char code);
+
+//Créer un compte
+int creercompte(username, code);
+
